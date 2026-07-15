9bea59b297ea743887778dec417a76b81f52cb8a
Meet-hybrid <philipmichaelonyekachi@gmail.com>
2026-07-15 16:26:16 +0100

    Completely remove celery files for Vercel deployment

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,1 @@
-# Celery removed for Vercel deployment
-# Remove this file or comment out the import if not using celery
-
-# If you need celery in the future, uncomment:
-# from .celery import app as celery_app
-# __all__ = ("celery_app",)
+# Empty __init__.py file - celery imports removed for Vercel deployment
